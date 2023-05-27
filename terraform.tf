7285a0c8f1b9730ce8e2c6bb419be027b58c284b
Stacey <getatmexpress@gmail.com>
2023-05-26 19:14:25 -0600

    Edited

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,11 @@
-#terraform {
-#required_version = ">= 1.0.0"
-#}
-
 terraform {
-required_version = ">= 1.0.0"
 required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 3.0"
-}
+  aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+http = {
+source = "hashicorp/http"
+version = "~> 3.3.0"
 }
 }
