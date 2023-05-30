218d13fcd293aaba09622fa357f5b25dcc4ae28e
Stacey <getatmexpress@gmail.com>
2023-05-30 17:59:26 -0600

    added

---
+++ delete4.tf
@@ -0,0 +1,21 @@
+# Leave the first part of the block unchanged and create our `local-exec
+` provisioner
+provisioner "local-exec" {
+command = "chmod 600 ${local_file.private_key_pem.filename}"
+}
+provisioner "remote-exec" {
+inline = [
+"sudo rm -rf /tmp",
+"sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp
+",
+"sudo sh /tmp/assets/setup-web.sh",
+]
+}
+tags = {
+Name = "Web EC2 Server"
+}
+lifecycle {
+ignore_changes = [security_groups]
+}
+}
+
