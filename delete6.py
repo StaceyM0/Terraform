af16f68290c10cf18b9df80ae70747669aacccfb
Stacey <getatmexpress@gmail.com>
2023-06-03 19:14:25 -0600

    Edited

--- delete6.py
+++ delete6.py
@@ -7,16 +7,3 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "Mywebsite" {
-  ami           = "ami-0715c1897453cabd1"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
