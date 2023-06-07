292721dbc2987e1137ab5e28b20a5e1cc47e7448
Stacey <getatmexpress@gmail.com>
2023-06-06 18:03:49 -0600

    Added

--- main3.tf
+++ main3.tf
@@ -1,17 +1,4 @@
-l_file" "private_key_pem" {
-  content  = tls_private_key.generated.private_key_pem
-  filename = "MyAWSKey.pem"
-}
 
-resource "aws_key_pair" "generated" {
-  key_name   = "MyAWSKey"
-  public_key = tls_private_key.generated.public_key_openssh
-}
-
-resource "aws_security_group" "ingress-ssh" {
-  name   = "allow-all-ssh"
-  vpc_id = aws_vpc.vpc.id
-  ingress {
     cidr_blocks = [
       "0.0.0.0/0"
     ]
